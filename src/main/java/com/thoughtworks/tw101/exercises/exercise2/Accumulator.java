ed88c5e529743d2e62f88c5d81d8912270c1b9f0
bodhello <bodhi.alarcon@gmail.com>
2018-09-11 21:58:40 -0400

    Complete Exercise 2

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,18 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+
+    private int value;
+
+    public Accumulator() {
+        value = 0;
+    }
+
+    void increment(){
+        ++value;
+    }
+
+    void total(){
+        System.out.println(value);
+    }
 }
