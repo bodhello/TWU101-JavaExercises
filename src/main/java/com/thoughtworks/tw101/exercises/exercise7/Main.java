21b0face6ef5111cd515c03e8066437bf7597139
bodhello <bodhi.alarcon@gmail.com>
2018-09-14 17:54:55 -0400

    Complete Exercise 7

--- src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
@@ -4,9 +4,46 @@ package com.thoughtworks.tw101.exercises.exercise7;
 // right then tell them they win, otherwise tell them if they guessed too high or low. Keep asking the user to guess
 // until they get the right answer. Use classes to separate the different concerns of this program.
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        GuessingGame newgame = new GuessingGame();
+        newgame.startGame();
+    }
+}
 
+class GuessingGame {
+    private int answer;
+    private boolean gameOver = true;
+
+    GuessingGame() {
+        this.answer = (int) (Math.random() * 100) + 1;
+    }
+
+    public void startGame(){
+        Scanner reader = new Scanner(System.in);
+        int guess;
+        gameOver = false;
+        while (!gameOver) {
+            System.out.print("Please guess a number between 1 and 100: ");
+            guess = reader.nextInt();
+            gameOver = guessNum(guess);
+        }
+    }
+
+    public boolean guessNum(int guess){
+        if (guess == this.answer){
+            System.out.println("Congrats! The correct number was " + this.answer + ". \nYOU WIN");
+            return true;
+        }
+        else if (guess < this.answer){
+            System.out.println("Your guess is TOO LOW. Try again!");
+        }
+        else {
+            System.out.println("Your guess is TOO HIGH. Try again!");
+        }
+        return false;
     }
 }
